014258fed3fb877416c04d5762f06dbdfe6fd5b8
ward <wardforthmann@gmail.com>
2017-01-10 23:38:50 -0800

    pulled out post and get payload

--- gobble.go
+++ gobble.go
@@ -21,37 +21,41 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Route => handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+	e.GET("/", handleGet)
 
-	e.POST("/", func(c echo.Context) error {
-
-		t := time.Now()
-		fo, err := os.Create(t.Format("2006-01-02 15.04.05"))
-		if err != nil {
-			panic(err)
-		}
-		// close fo on exit and check for its returned error
-		defer func() {
-			if err := fo.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		w := bufio.NewWriter(fo)
-		defer w.Flush()
-
-		for k, v := range c.Request().Header {
-			fmt.Fprintln(w, k + ":", strings.Join(v, ","))
-		}
-
-		fmt.Fprintln(w)
-		io.Copy(w, c.Request().Body)
-
-		return c.String(http.StatusOK, t.Format("2006-01-02 15:04:05"))
-	})
+	e.POST("/", handlePostPayload)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handlePostPayload(c echo.Context) error {
+
+	t := time.Now()
+	fo, err := os.Create(t.Format("2006-01-02 15.04.05"))
+	if err != nil {
+		panic(err)
+	}
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	w := bufio.NewWriter(fo)
+	defer w.Flush()
+
+	for k, v := range c.Request().Header {
+		fmt.Fprintln(w, k + ":", strings.Join(v, ","))
+	}
+
+	fmt.Fprintln(w)
+	io.Copy(w, c.Request().Body)
+
+	return c.String(http.StatusOK, t.Format("2006-01-02 15:04:05"))
+}
+
+func handleGet(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!\n")
 }
